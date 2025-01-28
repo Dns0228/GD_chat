0863412ab1ed5f128445cb003aab26bb23b6db76
Dns0228 <64970582+Dns0228@users.noreply.github.com>
2025-01-28 12:56:33 +0300

    вернул модель

--- bot/config.py
+++ bot/config.py
@@ -9,6 +9,6 @@ if not TELEGRAM_BOT_TOKEN:
     raise ValueError("Токен Telegram-бота не найден. Убедитесь, что он указан в .env.")
 
 # Настройки модели
-MODEL_NAME = "ai-forever/ruGPT-3"
+MODEL_NAME = "sberbank-ai/rugpt3small_based_on_gpt2"
 MAX_LENGTH = 750  # Максимальная длина ответа
 TEMPERATURE = 0.5  # Креативность модели (от 0 до 1)
